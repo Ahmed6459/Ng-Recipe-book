a9e561bc5ec9f00a763021027b102ecb6f2d4e7b
Ahmed Salah <salaheltantawy@gmail.com>
2022-02-19 00:43:15 +0200

    first commit

---
+++ src/app/recipes/recipe.routeing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeResolverService } from "./recipe-resolver-service";
+import { RecipesComponent } from "./recipes.component";
+import { StartToSelectComponent } from "./start-to-select/start-to-select.component";
+
+
+
+const routes:Routes=[
+    {path:"", component:RecipesComponent,
+    children:[
+        {path:"",component:StartToSelectComponent},
+        {path:"new",component:RecipeEditComponent},
+        {path:":id",component:RecipeDetailsComponent,resolve:[RecipeResolverService]},
+        {path:":id/edit", component:RecipeEditComponent,resolve:[RecipeResolverService]}
+    ]}
+]
+
+@NgModule({
+
+    imports:[RouterModule.forChild(routes)],
+    exports:[RouterModule]
+
+})
+export class RecipeRouteingModule{}
